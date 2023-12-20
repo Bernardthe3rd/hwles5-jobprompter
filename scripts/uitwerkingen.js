933e93833a9436f1ca69947772d3a21bd514a283
Bernardthe3rd <bammeijer@outlook.com>
2023-12-20 12:20:06 +0100

    finished hwles5

--- scripts/uitwerkingen.js
+++ scripts/uitwerkingen.js
@@ -51,5 +51,62 @@ if (userInput === 'marketing') {
 // }
 
 // opdracht 4a
-const employeeNumber = userInput['numberOfEmployees']
-console.log(userInput+' is een leuke afdeling om te werken. Er werken op dit moment '+employeeNumber+' medewerkers');
+if (userInput === 'marketing'){
+    console.log(userInput+' is een leuke afdeling om te werken. Er werken op dit moment '+departments.marketing.numberOfEmployees+' medewerkers');
+} else if (userInput === 'sales') {
+    console.log(userInput+' is een leuke afdeling om te werken. Er werken op dit moment '+departments.sales.numberOfEmployees+' medewerkers');
+} else if (userInput === 'customer-service') {
+    console.log(userInput+' is een leuke afdeling om te werken. Er werken op dit moment '+departments["customer-service"].numberOfEmployees+' medewerkers');
+} else {
+    console.error('Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.');
+    document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
+}
+
+// opdracht 4b + c
+const inputFlex = userInput
+
+const userInputFlex = prompt('Je koos '+userInput+'.'+ 'Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in. \n '+
+    '0: '+departments[inputFlex].jobs[0].title+' \n '+
+    '1: '+departments[inputFlex].jobs[1].title+' \n '+
+    '2: '+departments[inputFlex].jobs[2].title+' \n '+
+    '3: '+departments[inputFlex].jobs[3].title);
+
+switch (userInputFlex) {
+    case '0':
+        console.log('Je koos '+departments[inputFlex].jobs[0].title+'.'+' Een uitdagende rol! '+departments[inputFlex].jobs[0].description);
+        document.getElementById('role-title').textContent = departments[inputFlex].jobs[userInputFlex].title;
+        document.getElementById('department-description').textContent = departments[inputFlex].description;
+        document.getElementById('role-description').textContent = departments[inputFlex].jobs[userInputFlex].description;
+        break;
+    case '1':
+        console.log('Je koos '+departments[inputFlex].jobs[1].title+'.'+' Een uitdagende rol! '+departments[inputFlex].jobs[1].description);
+        document.getElementById('role-title').textContent = departments[inputFlex].jobs[userInputFlex].title;
+        document.getElementById('department-description').textContent = departments[inputFlex].description;
+        document.getElementById('role-description').textContent = departments[inputFlex].jobs[userInputFlex].description;
+        break;
+    case '2':
+        console.log('Je koos '+departments[inputFlex].jobs[2].title+'.'+' Een uitdagende rol! '+departments[inputFlex].jobs[2].description);
+        document.getElementById('role-title').textContent = departments[inputFlex].jobs[userInputFlex].title;
+        document.getElementById('department-description').textContent = departments[inputFlex].description;
+        document.getElementById('role-description').textContent = departments[inputFlex].jobs[userInputFlex].description;
+        break;
+    case '3':
+        console.log('Je koos '+departments[inputFlex].jobs[3].title+'.'+' Een uitdagende rol! '+departments[inputFlex].jobs[3].description);
+        document.getElementById('role-title').textContent = departments[inputFlex].jobs[userInputFlex].title;
+        document.getElementById('department-description').textContent = departments[inputFlex].description;
+        document.getElementById('role-description').textContent = departments[inputFlex].jobs[userInputFlex].description;
+        break;
+    default:
+        console.error('Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.');
+        document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
+}
+
+// opdracht 5a
+// document.getElementById('role-title').textContent = 'Benjamin';
+// document.getElementById('department-description').textContent = 'Lasagna';
+
+// opdracht 5b
+// document.getElementById('role-title').textContent = departments[inputFlex].jobs[userInputFlex].title;
+// document.getElementById('department-description').textContent = departments[inputFlex].description;
+// document.getElementById('role-description').textContent = departments[inputFlex].jobs[userInputFlex].description;
+// document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
